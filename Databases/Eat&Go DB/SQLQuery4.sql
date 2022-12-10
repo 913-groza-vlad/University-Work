f07dd1569e7ad5224a333e6cdca0c59daabb2a9a
Vlad Groza <vlad.andrei.groza@stud.ubbcluj.ro>
2022-12-10 20:52:00 +0200

    Update SQLQuery4.sql

--- Databases/Eat&Go DB/SQLQuery4.sql
+++ Databases/Eat&Go DB/SQLQuery4.sql
@@ -1,5 +1,5 @@
  
-CREATE PROCEDURE addTables (@tableName varchar(20)) AS
+CREATE PROCEDURE addTable (@tableName varchar(20)) AS
 	IF @tableName in (SELECT Name From Tables)
 	BEGIN
 		PRINT 'Table already added in Tables'
@@ -9,3 +9,22 @@ CREATE PROCEDURE addTables (@tableName varchar(20)) AS
 
 go
 
+CREATE PROCEDURE addView (@viewName varchar(20)) AS
+	IF @viewName in (SELECT Name From Views)
+	BEGIN
+		PRINT 'View already added in Views'
+		return
+	END
+	INSERT INTO Views(Name) values (@viewName)
+
+go
+
+CREATE PROCEDURE addTest (@testName varchar(20)) AS
+	IF @testName in (SELECT Name From Tests)
+	BEGIN
+		PRINT 'Test already added in Tests'
+		return
+	END
+	INSERT INTO Tests(Name) values (@testName)
+
+go
